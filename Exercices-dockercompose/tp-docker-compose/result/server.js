e97d039439ab6b61fc6bdfbc65bb0c60e2dcd7f4
ihab abadi <ihab@utopios.net>
2024-07-09 14:06:37 +0200

    correction TP docker compose vente

--- Exercices-dockercompose/tp-docker-compose/result/server.js
+++ Exercices-dockercompose/tp-docker-compose/result/server.js
@@ -10,7 +10,7 @@ var express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io')(server);
 
-io.set('transports', ['polling']);
+//io.set('transports', ['polling']);
 
 var port = process.env.PORT || 4000;
 
